06fabb2782a9f50eb12e5756287c566142674ea8
ajibadedapo <ajibadehammed@gmail.com>
2019-08-24 08:04:19 +0100

    connecting mongo database

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://aj_dapo:ajibadedapo@cluster0-akhzz.mongodb.net/test?retryWrites=true&ssl=false&w=majority'
+};
--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
+const db =require('./config/keys').mongoURI;
+
+mongoose.connect(db, {useNewUrlParser: true })
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err)
+);
+
 app.get('/', (req, res) => res.send('Jesu my people !') );
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 7383;
+
+
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
